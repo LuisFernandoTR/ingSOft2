b0c7478795b92c9398266f2d23106d1ef3a68770
Luis Fernando Talavera Ramirez <87208472+LuisFernandoTR@users.noreply.github.com>
2023-05-30 23:48:17 -0600

    Add files via upload

---
+++ Ventas/src/java/config/Conexion.java
@@ -0,0 +1,45 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author monts
+ */
+public class Conexion {
+        private static final String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/bd_sistemaventas";
+	private static final String USUARIO = "root";
+	private static final String CLAVE = "";
+
+	static {
+		try {
+			Class.forName(CONTROLADOR);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al cargar el controlador");
+			e.printStackTrace();
+		}
+	}
+	
+	public  Connection Conexion() {
+		Connection conexion = null;
+		
+		try {
+			conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+			System.out.println("Conexión OK");
+
+		} catch (SQLException e) {
+			System.out.println("Error en la conexión");
+			e.printStackTrace();
+		}
+		
+		return conexion;
+	}
+}
